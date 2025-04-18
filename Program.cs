3cfadc91a8c50b62e06ddb877179403971a3f9c9
Christian Rodriguez <christianarg@outlook.com>
2025-04-18 10:43:06 +0200

    con agent

--- Program.cs
+++ Program.cs
@@ -2,20 +2,40 @@
 #pragma warning disable CS8321 // Local function is declared but never used
 
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
 using Microsoft.SemanticKernel.ChatCompletion;
 
-await BasicQALoop();
+await BasicQALootAgentFramework();
+//await BasicQALoopChat();
+//await SimplestSample();
 
-async Task BasicQALoop()
+async Task BasicQALootAgentFramework()
 {
-    var kernel = Kernel.CreateBuilder()
-        .AddOllamaChatCompletion(modelId: "llama3.2", endpoint: new("http://localhost:11434"))
-        .Build();
+    var kernel = DefaultOllamaKernel();
+    ChatCompletionAgent agent = new()
+    {
+        Name = null,
+        Instructions = null,
+        Kernel = kernel
+    };
+    ChatHistoryAgentThread agentThread = new();
+    while (true)
+    {
+        Console.Write("User>");
+        var userInput = Console.ReadLine()!;
+        var result = await agent.InvokeAsync(userInput, agentThread).ToListAsync();
+        Console.WriteLine($"Assistant> {result.First().Message}");
+    }
+}
+
+async Task BasicQALoopChat()
+{
+    var kernel = DefaultOllamaKernel();
 
     var chatService = kernel.GetRequiredService<IChatCompletionService>();
     ChatHistory chat = new();
 
-    while(true)
+    while (true)
     {
         Console.Write("User>");
         var userInput = Console.ReadLine()!;
@@ -24,18 +44,22 @@ async Task BasicQALoop()
         Console.WriteLine($"Assistant> {result}");
         chat.AddAssistantMessage(result.ToString());
     }
-    
+
 }
 
 async Task SimplestSample()
 {
-    var kernel = Kernel.CreateBuilder()
-        .AddOllamaChatCompletion(modelId: "llama3.2", endpoint: new("http://localhost:11434"))
-        .Build();
-
+    var kernel = DefaultOllamaKernel();
     var result = await kernel.InvokePromptAsync("Hola crack");
     Console.WriteLine(result);
 
+}
+
+Kernel DefaultOllamaKernel()
+{
+    return Kernel.CreateBuilder()
+        .AddOllamaChatCompletion(modelId: "llama3.2", endpoint: new("http://localhost:11434"))
+        .Build();
 }
 
 Console.ReadLine();
