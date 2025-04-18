042d7d0b87bf6963ece5d1c7025775d4606b818f
Christian Rodriguez <christianarg@outlook.com>
2025-04-18 10:02:00 +0200

    quick sample with SK and ollama

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+using Microsoft.SemanticKernel;
+
+var kernel = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion(modelId: "llama3.2", endpoint: new("http://localhost:11434"))
+    .Build();
+
+var result = await kernel.InvokePromptAsync("Hola crack");
+Console.WriteLine(result);
+Console.ReadLine();
